103e65bcb25a8e276b821933e89e229355538119
Michael Fry <michael.david.fry@gmail.com>
2023-04-11 12:47:22 -0400

    Create single-ping.py

---
+++ single-ping.py
@@ -0,0 +1,21 @@
+import subprocess
+
+# Read the list of IP addresses from an external text file
+with open('ip_list.txt') as f:
+    ip_list = [line.strip() for line in f]
+
+# Get the list of network interfaces
+interfaces = subprocess.check_output(['ifconfig', '-s']).decode().splitlines()[1:]
+
+# Loop through each interface
+for interface in interfaces:
+    interface_name = interface.split()[0]
+    
+    # Loop through each IP address in the list
+    for ip in ip_list:
+        # Execute the ping command for the IP address on the current interface
+        try:
+            subprocess.check_output(['ping', '-I', interface_name, '-c', '1', '-W', '1', ip])
+            print(f'Ping successful: {ip} on {interface_name}')
+        except subprocess.CalledProcessError:
+            print(f'Ping failed: {ip} on {interface_name}')
